a252401e64ef240d7a97becc2a4c30f17f048fbe
araki <deliciouslife.of.hapken.103@gmail.com>
2022-03-02 17:14:54 +0900

    add new file

--- memo.rb
+++ memo.rb
@@ -7,7 +7,7 @@ memo_type = gets.to_i
 
 if memo_type == 1
   CSV.open('New.csv','w') do |csv|
-    puts "拡張子を除いた新規メモのファイル名を入力してください。"
+    puts "新規メモのファイル名を入力してください（拡張子は除く）"
     Newfile_name = gets.chomp
     File.rename("New.csv", "#{Newfile_name}.csv")
 
@@ -22,14 +22,18 @@ if memo_type == 1
   end
 
 elsif memo_type == 2
-  puts "既存メモのファイル名を入力してください。"
+  puts "既存メモのファイル名を入力してください。（拡張子は除く）"
   Existing_filename = gets.chomp
-  
-  CSV.open(Existing_filename,'a') do |csv|
+  data = CSV.read("#{Existing_filename}.csv", headers: true)
+  puts "-------------------------------------------"
+  puts "ー既存メモの内容ー"
+  puts ""
+  puts data
+  CSV.open("#{Existing_filename}.csv",'a') do |csv|
 
     puts "-------------------------------------------"
-    puts "これで準備完了です。"
-    puts "メモしたい内容を記入してください。追記します。"
+    puts "準備完了です。"
+    puts "メモしたい内容を下記に記入してください。追記します。"
     puts "入力完了後にCtrl + Dを押すと、保存して終了します。"
     puts "-------------------------------------------"
 
